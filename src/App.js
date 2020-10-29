9d66d5685251b592e15312b76cde8d2e4f0df6f0
Rauru <raul_lopez@unitec.edu>
2020-10-29 01:39:49 -0600

    styles font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Main from "./components/Main";
 import Header from "./components/Header";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 //import "./styles/styles.scss";
 
