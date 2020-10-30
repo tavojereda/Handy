90dc2a2a95f3131b88becb17c50483674ffdc860
gustavo jereda <tavo_jereda@hotmail.com>
2020-10-30 02:37:51 -0600

    footer font changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,38 +3,38 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Footer = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
+    <MDBFooter className="font-small pt-4">
+      <MDBContainer fluid className="text-center text-md-left footer-text-body">
         <MDBRow>
           <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
+            <h5 className="title footer-text-title">Footer Content</h5>
             <p>
               Here you can use rows and columns here to organize your footer
               content.
             </p>
           </MDBCol>
           <MDBCol md="6">
-            <h5 className="title">Links</h5>
+            <h5 className="title footer-text-title">Compañia</h5>
             <ul>
               <li className="list-unstyled">
-                <a href="#!">Link 1</a>
+                <a href="#!">Contactanos</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 2</a>
+                <a href="#!">Trabajos</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 3</a>
+                <a href="#!">Acerca de Handy</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 4</a>
+                <a href="#!">Invercionistas</a>
               </li>
             </ul>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-      <div className="footer-copyright text-center py-3">
+      <div className="footer-copyright text-center py-3 ">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+          &copy; {new Date().getFullYear()} Copyright: <a href="www.handy.com">Handy </a>
         </MDBContainer>
       </div>
     </MDBFooter>
